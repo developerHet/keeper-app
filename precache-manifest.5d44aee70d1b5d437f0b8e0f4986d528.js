b3c17769c8012dc720ab14e0ad821322c3610a15
Het <hetpatel2312.hp@gmail.com>
2022-05-25 10:38:26 +0530

    Updates

---
+++ precache-manifest.5d44aee70d1b5d437f0b8e0f4986d528.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aded6320f3f381c9741b2ba0f0efe620",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/keeper-app/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "30988c5ff00fd5a3ff83",
+    "url": "/keeper-app/static/js/main.55d25570.chunk.js"
+  },
+  {
+    "revision": "ed9243ad83c12b8fd98c",
+    "url": "/keeper-app/static/js/runtime-main.604284a8.js"
+  }
+]);
